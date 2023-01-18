b8bc4c9a2ec90888e8708d10b26c610c7fcf06fd
Katrina Harradine <katrina@revmo.app>
2023-01-18 01:19:03 -0400

    Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
-    return <h1> Welcome </h1>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/" component={Home} />
+            <Route path="*">Ups</Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
-// import logo from './logo.svg';
-// import './App.css';
-// import React from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
