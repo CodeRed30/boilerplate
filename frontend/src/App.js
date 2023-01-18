2e00d953e3486d810377b489ed1cd9e73c588503
Katrina Harradine <katrina@revmo.app>
2023-01-18 01:39:07 -0400

    Fix react-router-dom version issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
@@ -9,15 +9,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <Router>
           <Routes>
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="*">Ups</Route>
           </Routes>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
